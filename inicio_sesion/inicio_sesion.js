3ae897641cfcda42448fc4baa5fd5701b3d30ba0
khaos-23 <zazopro23@gmail.com>
2025-01-29 16:10:52 +0000

    29/01/2025

--- inicio_sesion/inicio_sesion.js
+++ inicio_sesion/inicio_sesion.js
@@ -7,16 +7,48 @@ document.getElementById('tutor').addEventListener('change', function() {
         cursoTutorField.style.display = 'block';
     }
 });
+
 document.getElementById('add-curso').addEventListener('click', function() {
     const container = document.getElementById('cursos-ensenados-container');
     const newCurso = document.createElement('div');
     newCurso.classList.add('curso-ensenado');
     newCurso.innerHTML = `
-        <label for="cursos-ensenados">Curso donde enseña:</label>
-        <input type="text" name="cursos-ensenados">
-        
-        <label for="clases-ensenadas">Clases donde enseña (máximo 4):</label>
-        <input type="text" name="clases-ensenadas">
+        <label for="cursos-ensenados">Curso i clase donde enseña:</label>
+                        <select name="cursos-ensenados">
+                            <option value="1ESO">1ESO A</option>
+                            <option value="1ESO">1ESO B</option>
+                            <option value="1ESO">1ESO C</option>
+    
+                            <option value="2ESO">2ESO A</option>
+                            <option value="2ESO">2ESO B</option>
+                            <option value="2ESO">2ESO C</option>
+                            
+                            <option value="3ESO">3ESO A</option>
+                            <option value="3ESO">3ESO B</option>
+                            <option value="3ESO">3ESO C</option>
+                            <option value="3ESO">3ESO D</option>
+    
+                            <option value="4ESO">4ESO A</option>
+                            <option value="4ESO">4ESO B</option>
+                            <option value="4ESO">4ESO C</option>
+                            <option value="4ESO">4ESO D</option>
+    
+                            <option value="BATXILLERATO_CIENCIAS_TECNOLOGIA">BATXILLERATO DE CIENCIAS I TECNOLOGIA 1r</option>
+                            <option value="BATXILLERATO_CIENCIAS_TECNOLOGIA">BATXILLERATO DE CIENCIAS I TECNOLOGIA 2n</option>
+    
+                            <option value="BATXILLERATO_HUMANIDAD_CIENCIAS_SOCIALES">BATXILLERATO DE HUMANIDAD I CIENCIAS SOCIALES 1r</option>
+                            <option value="BATXILLERATO_HUMANIDAD_CIENCIAS_SOCIALES">BATXILLERATO DE HUMANIDAD I CIENCIAS SOCIALES 2n</option>
+    
+                            <option value="DAW">DAW 1r</option>
+                            <option value="DAW">DAW 2n</option>
+    
+                            <option value="ASIX">ASIX 1r</option>
+                            <option value="ASIX">ASIX 2n</option>
+    
+                            <option value="SMIX">SMIX 1r</option>
+                            <option value="SMIX">SMIX 2n</option>
+                        </select>
+    
         
         <label for="asignaturas">Asignaturas que enseña:</label>
         <input type="text" name="asignaturas">
@@ -30,20 +62,20 @@ document.getElementById('role').addEventListener('change', function() {
     const profesorFields = document.getElementById('profesor-fields');
     
     if (this.value === 'alumno') {
-        alumnoFields.style.display = 'block';
+        alumnoFields.style.display = 'flex';
         profesorFields.style.display = 'none';
-    } else {
+    } if (this.value === 'profesor'){
         alumnoFields.style.display = 'none';
-        profesorFields.style.display = 'block';
+        profesorFields.style.display = 'flex';
     }
 });
 
 // Cambiar entre inicio de sesión y registro
 document.getElementById('switch-to-register').addEventListener('click', function() {
-    document.getElementById('login-section').style.display = 'none';
-    document.getElementById('register-section').style.display = 'block';
+    document.getElementById('login-section').style.display = 'none'; //El formulario login desepareze cambiado su valor de display a none
+    document.getElementById('register-section').style.display = 'block'; //El formulario de registro apareze cambiado su valor de display al que estoy usando
 });
-
+//Lo mismo pero viceversa
 document.getElementById('switch-to-login').addEventListener('click', function() {
     document.getElementById('register-section').style.display = 'none';
     document.getElementById('login-section').style.display = 'block';
