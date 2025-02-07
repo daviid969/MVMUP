99b3bc08121409870183e964aa95e3ed57838364
khaos-23 <zazopro23@gmail.com>
2025-02-07 18:35:15 +0000

    07/02/2025

--- pagina_almacenamiento/upload.php
+++ pagina_almacenamiento/upload.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST["submit"])) {
-  $target_dir = "uploads/";
+  $target_dir = "home/hosting2/Descargas/";
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "El archivo ". basename( $_FILES["fileToUpload"]["name"]). " ha sido subido.";
