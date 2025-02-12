9c75caf3a65e826f4f39157fcc61485d67279f0b
khaos-23 <zazopro23@gmail.com>
2025-02-12 15:15:51 +0000

    27/01/25/

--- pagina_almacenamiento/upload.php
+++ pagina_almacenamiento/upload.php
@@ -1,7 +1,10 @@
 <?php
 if(isset($_POST["submit"])) {
+
   $target_dir = "home/debian/Descargas/";
+  
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "El archivo ". basename( $_FILES["fileToUpload"]["name"]). " ha sido subido.";
   } else {
