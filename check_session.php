e3fb2a1d742a3150f57fa71cc42f16906623bf0b
khaos-23 <zazopro23@gmail.com>
2025-02-15 19:16:48 +0000

    27/01/25/

--- check_session.php
+++ check_session.php
@@ -3,13 +3,17 @@ session_start();
 
 $response = [
     'loggedIn' => false,
-    'username' => ''
+    'username' => '',
+    'redirect' => false // Nuevo campo para indicar si se debe redirigir
 ];
 
 if (isset($_SESSION['user_id'])) {
     $response['loggedIn'] = true;
     $response['username'] = $_SESSION['username'];
+} else {
+    $response['redirect'] = true; // Indicar que se debe redirigir
 }
 
+header('Content-Type: application/json'); // Especificar que la respuesta es JSON
 echo json_encode($response);
 ?>
