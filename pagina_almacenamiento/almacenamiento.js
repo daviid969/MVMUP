8f5c11c5694a701a2240f3a0f3996064aca5b2da
khaos-23 <zazopro23@gmail.com>
2025-02-21 18:10:37 +0000

    21/02/2025

--- pagina_almacenamiento/almacenamiento.js
+++ pagina_almacenamiento/almacenamiento.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/list_files.php')
+    fetch('/pagina_almacenamiento/list_files.php')
         .then(response => response.json())
         .then(files => {
             const fileList = document.getElementById('fileList');
             files.forEach(file => {
                 const listItem = document.createElement('li');
                 listItem.className = 'list-group-item';
-                listItem.innerHTML = `<a href="/download.php?file=${encodeURIComponent(file)}">${file}</a>`;
+                listItem.innerHTML = `<a href="/pagina_almacenamiento/download.php?file=${encodeURIComponent(file)}">${file}</a>`;
                 fileList.appendChild(listItem);
             });
         })
