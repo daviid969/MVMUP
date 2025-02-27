930062755106ca128f478a6dbda3370f5f1289b7
khaos-23 <zazopro23@gmail.com>
2025-02-27 15:01:04 +0000

    27/02/2025

--- pagina_almacenamiento/list_files.php
+++ pagina_almacenamiento/list_files.php
@@ -1,5 +1,12 @@
 <?php
-$directory = "/mvmup_stor/";
-$files = array_diff(scandir($directory), array('.', '..'));
+$sql="select * from usuarios where id ='".$_GET["id"]."' ";
+$usuario = $_GET["id"];
+$nombre_carpeta = "/mvmup_stor/".$usuario."";
+
+if(!is_dir($nombre_carpeta)){
+@mkdir($nombre_carpeta, 0700);
+}
+
+$files = array_diff(scandir($nombre_carpeta), array('.', '..'));
 echo json_encode(array_values($files));
 ?>
