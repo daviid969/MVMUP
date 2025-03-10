43046a2a410eb277f78ab8f66b2af1c60c82b010
khaos-23 <zazopro23@gmail.com>
2025-03-10 19:18:55 +0000

    10/03/2025

--- inicio_sesion/registro.php
+++ inicio_sesion/registro.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
    // Insertar usuario en la base de datos
    $sql = "INSERT INTO usuarios (username, nombre, apellidos, email, curso, password)
-           VALUES ($password, $curso, $email, $apellidos, $nombre, $username)";
+           VALUES ('$password', '$curso', '$email', '$apellidos', '$nombre', '$username')";
    $stmt = $conn->prepare($sql);
    if ($stmt === false) {
        echo json_encode(["success" => false, "message" => "Error en la preparación de la consulta: " . $conn->error]);
