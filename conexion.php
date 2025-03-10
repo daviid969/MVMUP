3c3dfd6ed1182ebc5331d26d4615d03ec0105286
khaos-23 <zazopro23@gmail.com>
2025-03-10 19:04:12 +0000

    10/03/2025

--- conexion.php
+++ conexion.php
@@ -1,10 +1,14 @@
 <?php
+ini_set('display_errors', 1);
 
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
 require_once('/config.php');
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+die;
 // Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
