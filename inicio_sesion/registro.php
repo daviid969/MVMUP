e588ef26e5efd3cd52b4e13618a7aeaaf367bd53
khaos-23 <zazopro23@gmail.com>
2025-03-10 18:21:34 +0000

    10/03/2025

--- inicio_sesion/registro.php
+++ inicio_sesion/registro.php
@@ -7,10 +7,15 @@ $username = "mvmup_root";
 $password = "mvmup@KCIPDE";
 $dbname = "mvmup";
 
+
+// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
 if ($conn->connect_error) {
-   die("Conexión fallida: " . $conn->connect_error);
+    die("Conexión fallida: " . $conn->connect_error);
 }
+
 // Configuración de la base de datos
 //require_once "conexion.php";
 
