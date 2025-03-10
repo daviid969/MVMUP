301915ffb72f9d436436e4cc91dd387251d59f50
khaos-23 <zazopro23@gmail.com>
2025-03-10 18:45:23 +0000

    10/03/2025

--- conexion.php
+++ conexion.php
@@ -1,9 +1,10 @@
 <?php
-echo "socoorro"; die;
+
 require_once('config.php');
+
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+echo "socoorro"; die;
 // Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
