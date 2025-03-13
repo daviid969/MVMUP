d5f3f0fe1389407f139d20d7edb206043e75c580
khaos-23 <zazopro23@gmail.com>
2025-03-13 14:28:35 +0000

    13/03/2025

--- pagina_almacenamiento/list_files.php
+++ pagina_almacenamiento/list_files.php
@@ -1,5 +1,7 @@
 <?php
-$directory = "/mvmup_stor/";
+require_once "/inicio_sesion/inicio_sesion.php";
+
+$directory = "/mvmup_stor/$email";
 $files = array_diff(scandir($directory), array('.', '..'));
 echo json_encode(array_values($files));
 ?>
