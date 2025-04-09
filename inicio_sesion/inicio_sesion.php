facec22569cff6153f610d04ba2fd888f6d045c4
khaos-23 <zazopro23@gmail.com>
2025-04-09 14:00:38 +0000

    9/04/2025

--- inicio_sesion/inicio_sesion.php
+++ inicio_sesion/inicio_sesion.php
@@ -7,14 +7,14 @@ require_once "../conexion.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    phpinfo();
+    
 
     // Buscar usuario en la base de datos
     $sql = "SELECT id, username, directory, password FROM usuarios WHERE email = $email)";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $result = $stmt->get_result();
-
+    phpinfo();
     if ($result->num_rows > 0) {
        $row = $result->fetch_assoc();
 
