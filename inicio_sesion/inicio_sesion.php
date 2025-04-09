850c928602654fe61647570f5911075d1edcbd2b
khaos-23 <zazopro23@gmail.com>
2025-04-09 14:29:51 +0000

    9/04/2025

--- inicio_sesion/inicio_sesion.php
+++ inicio_sesion/inicio_sesion.php
@@ -35,9 +35,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($directory == 1){
                 header('Location: /index.html');
             } else {
-                
-                mkdir /mvmup/$id;
                 phpinfo();
+                mkdir /mvmup/$id;
+                
                 $update_sql = "UPDATE usuarios SET directory = 1 WHERE id = $id";
                 if ($conn->query($update_sql) === TRUE) {
                     echo "Correo actualizado correctamente.";
