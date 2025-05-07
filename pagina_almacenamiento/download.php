4316ce44d054136d5dce39692174e50c03f5fa91
khaos-23 <zazopro23@gmail.com>
2025-05-07 08:11:29 +0000

    07/05/2025

--- pagina_almacenamiento/download.php
+++ pagina_almacenamiento/download.php
@@ -8,13 +8,12 @@ if (isset($_GET['file'])) {
     $file = realpath($_GET['file']);
     $base_directory = realpath("/mvmup_stor/$id");
 
-    // Verificar si el archivo pertenece al usuario o está compartido con él
+    // Validar si el archivo está dentro del directorio base del usuario o está compartido
     $stmt = $conn->prepare("SELECT file_path FROM shared_files WHERE (shared_with_id = ? OR owner_id = ?) AND file_path = ?");
     $stmt->bind_param("iis", $id, $id, $file);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    // Validar si el archivo está dentro del directorio base del usuario o está compartido
     if ((strpos($file, $base_directory) === 0 && file_exists($file)) || $result->num_rows > 0) {
         if (file_exists($file)) {
             header('Content-Description: File Transfer');
