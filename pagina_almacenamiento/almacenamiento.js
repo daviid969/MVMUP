ca79fa69d5066e8fe40c9720db3c148514d5f8fa
khaos-23 <zazopro23@gmail.com>
2025-05-07 10:21:31 +0000

    07/05/2025

--- pagina_almacenamiento/almacenamiento.js
+++ pagina_almacenamiento/almacenamiento.js
@@ -302,14 +302,14 @@ function enterSharedFolder(folderPath) {
 function goBackSharedFolder() {
   if (sharedPathStack.length > 0) {
     sharedPathStack.pop(); // Eliminar la ruta actual
-    const previousPath = sharedPathStack.length > 0 ? sharedPathStack[sharedPathStack.length - 1] : null;
-
+    const previousPath = sharedPathStack.length > 0 ? sharedPathStack[sharedPathStack.length - 1] : '';
     if (previousPath) {
       enterSharedFolder(previousPath); // Volver a la carpeta anterior
     } else {
-      sharedPathStack = []; // Reiniciar la pila si estamos en la raíz
       loadSharedFiles(); // Cargar la lista inicial de archivos compartidos
     }
+  } else {
+    loadSharedFiles(); // Cargar la lista inicial de archivos compartidos
   }
 
   // Ocultar el botón si estamos en la raíz
