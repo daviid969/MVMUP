3a63a3aa7f11276c87f5e73db370f5b6547e97de
khaos-23 <zazopro23@gmail.com>
2025-05-07 08:21:02 +0000

    07/05/2025

--- pagina_almacenamiento/download_local.php
+++ pagina_almacenamiento/download_local.php
@@ -8,7 +8,8 @@ if (isset($_GET['file'])) {
     $base_directory = realpath("/mvmup_stor/$id");
 
     // Validar si el archivo está dentro del directorio base del usuario
-    if (strpos($file, $base_directory) === 0 && file_exists($file)) {
+    if ($file && strpos($file, $base_directory) === 0 && file_exists($file)) {
+        // Configurar cabeceras para la descarga
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename="' . basename($file) . '"');
@@ -16,6 +17,8 @@ if (isset($_GET['file'])) {
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
         header('Content-Length: ' . filesize($file));
+
+        // Leer y enviar el archivo
         readfile($file);
         exit;
     } else {
@@ -23,5 +26,9 @@ if (isset($_GET['file'])) {
         echo "No tienes permiso para descargar este archivo.";
         exit;
     }
+} else {
+    http_response_code(400);
+    echo "Archivo no especificado.";
+    exit;
 }
 ?>
