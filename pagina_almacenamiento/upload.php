310061432b639c7fa47d8c9398ecb027ba29dedc
khaos-23 <zazopro23@gmail.com>
2025-05-12 09:49:04 +0000

    12/05/2025

--- pagina_almacenamiento/upload.php
+++ pagina_almacenamiento/upload.php
@@ -7,33 +7,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['fileToUpload'])) {
     $base_directory = "/mvmup_stor/$id";
     $path = isset($_POST['path']) ? $_POST['path'] : '';
     $target_dir = realpath($base_directory) . '/' . trim($path, '/');
-
+    
     // Verificar que la ruta sea válida
     if (strpos($target_dir, realpath($base_directory)) !== 0) {
-        echo json_encode(["status" => "error", "message" => "Acceso no permitido"]);
+        echo "Acceso no permitido";
         exit;
     }
 
     // Crear el archivo de destino
     $target_file = $target_dir . '/' . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
 
     // Verificar si el archivo ya existe
     if (file_exists($target_file)) {
-        echo json_encode(["status" => "error", "message" => "El archivo ya existe"]);
+        echo "El archivo ya existe";
         exit;
     }
 
     // Verificar el tamaño del archivo (límite: 50MB)
     if ($_FILES["fileToUpload"]["size"] > 50 * 1024 * 1024) {
-        echo json_encode(["status" => "error", "message" => "Tu archivo supera el límite de 50MB."]);
+        echo "Tu archivo supera el límite de 50MB.";
         exit;
     }
 
     // Subir el archivo
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo json_encode(["status" => "success", "message" => "El archivo " . basename($_FILES["fileToUpload"]["name"]) . " ha sido subido con éxito."]);
+        header("Location: /pagina_almacenamiento/index.html?upload=success");
+        exit;
     } else {
-        echo json_encode(["status" => "error", "message" => "Hubo un error al subir tu archivo."]);
+        header("Location: /pagina_almacenamiento/index.html?upload=error");
+        exit;
     }
 }
 ?>
